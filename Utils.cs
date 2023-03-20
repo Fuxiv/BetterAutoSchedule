bd6049a841a00382d3fb4a369913a0c0a6fd43fe
Jakub Szymański <simple.jacob@icloud.com>
2023-03-21 00:29:46 +0100

    1

---
+++ Utils.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Utils
+{
+    static SQLiteConnection CreateConnection()
+    {
+        SQLiteConnection sqlite_conn;
+        sqlite_conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;");
+        return sqlite_conn;
+    }
+}
